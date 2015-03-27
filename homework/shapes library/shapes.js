ef664b939cff63dc530f99bf2faa708a61b15494
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-03-26 21:32:35 -0700

    made 2 more shapes
    made a sphere and a pyramid

--- homework/shapes library/shapes.js
+++ homework/shapes library/shapes.js
@@ -8,50 +8,63 @@ var Shapes = {
     /*
      * Returns the vertices for a small icosahedron.
      */
-    sphere: function () {
+    cylinder: function () {
         return{
             vertices: [
-            // [0,0.5,0],
-            // [0.05,0.45,0.1],
-            // [-0.05,0.45,0.1],
-            // [0.05,0.45,-0.1],
-            // [-0.05,0.45,-0.1],
-            // [0.15,0.45,0.0],
-            // [-0.15,0.45,0.0]
-
             ],
 
             indices: [
-            // [1,0,2],
-            // [2,0,3],
-            // [3,0,4],
-            // [4,0,5],
-            // [5,0,6],
-            // [6,0,1]
             ]
 
         }
     },
 
-    triangle: function (){
+    sphere: function (){
+        return{
+            vertices:[
+            ],
+            indices: [
+            ]
+        }
+    },
+
+    doublePyramid: function (){
         return{
             vertices:[
             [0,0,0],
-            [0.25,0.25,0],
-            [-0.25,0.25,0],
-            [0.30,0.25,0],
-            [0,-0.1,0],
-            [0.35,0.25,0]
-            [0,-0.15,0]
+            [0.1,0.1,0.1],
+            [-0.1,0.1,-0.1],
+            [-0.1,0.1,0.1],
+            [0.1,0.1,-0.1],
+            [0.1,-0.1,0.1],
+            [-0.1,-0.1,-0.1],
+            [-0.1,-0.1,0.1],
+            [0.1,-0.1,-0.1]
             ],
             indices: [
             [0,1,2],
-            [1,2,0],
-            [2,0,1],
-            [3,4,1]
+            [0,3,4],
+            [1,3,4],
+            [2,3,4],
+            [0,5,6],
+            [0,7,8],
+            [5,7,8],
+            [6,7,8]
             ]
         }
     },
+
+    // star:function(){
+    //     return{
+    //         vertices:[
+    //         [0.2,0.2,0.2],
+    //         [0.1,0.1,0.1],
+
+    //         ],
+    //         indices:[
+    //         ]
+    //     }
+    // }
 
 
     // icosahedron: function () {
@@ -173,34 +186,34 @@ var Shapes = {
 //         var angleInRadians = angleInDegrees * Math.PI / 180;
 //         var s = Math.sin(angleInRadians);
 //         var c = Math.cos(angleInRadians);
-//         console.log("s = " + s + " c = " + c);
+//         // console.log("s = " + s + " c = " + c);
 //     }
   
 
-    drawSphere: function(indexedVertices){
-        var center = [0,0,0]
-        var radius = 0.5
-        var l = 20
-        var angleInDegrees = 0
-        var angleInRadians = angleInDegrees * Math.PI / 180;
-        var s = Math.sin(angleInRadians);
-        var c = Math.cos(angleInRadians);
+    drawCylinder: function(indexedVertices){
+        var center = [0,0,0];
+        var radius = 0.5;
+        var l = 20;
+        var angleInRadians;
+        var s;
+        var c;
         // x= vertexArray[0]+ Math.cos(t) + Math.sin(t)q
         // for(var i = angleInDegrees;i===360;i+=10){
-        do{
-            ++angleInDegrees
-            angleInRadians = angleInDegrees * Math.PI / 180;
+        var MAGIC_NUMBER = 360;
+        var max = 2*Math.PI;
+        var incr = max/MAGIC_NUMBER;
+        for(angleInRadians = 0; angleInRadians < max; angleInRadians += incr) {
             s = Math.sin(angleInRadians);
             c = Math.cos(angleInRadians);
-            for(var i=radius;i>=-0.5;i-=0.05){
+            for(var i=radius;i>=-radius;i-=0.05){
                 indexedVertices.vertices.push([s/2,i,c/2])
             }
-        }while(angleInDegrees!==360)
-        console.log(indexedVertices.vertices)
-            for(var j=0;j<=indexedVertices.vertices.length;j++ ){
+        }
+        // console.log(indexedVertices.vertices)
+            for(var j=0;j<indexedVertices.vertices.length;j++ ){
                 indexedVertices.indices.push([j,l++,j])
             }
-        indexedVertices.vertices.length
+        //indexedVertices.vertices.length
         var result = [],
             i,
             j,
@@ -220,9 +233,87 @@ var Shapes = {
                 );
             }
         }
-        console.log(result)
+        // console.log(result)
         return result;
-    }
+    },
 
+    // drawSphere: function(indexedVertices){
+    //     var radius = 0.1
+    //     var center = [0,0,0]
+    //     var angleInDegrees = 0
+    //     do{
+    //         var angleInRadians = angleInDegrees * Math.PI / 180;
+    //         var s = Math.sin(angleInRadians);
+    //         var c = Math.cos(angleInRadians);
+    //         indexedVertices.vertices.push([s,0,c])
+    //     }while(angleInDegrees!==360)
+        
+
+
+    // }
+
+    drawSphere:function (indexedVertices){
+        var numberOfVertices=0;
+        var theta,phi;
+        for (var i=0; i<=64; i++) {   //loop on crowns
+            phi=Math.PI*i/64;         //compute lattitude
+
+            for (j=0; j<=32; j++) { //loop on bands
+                theta=2*Math.PI*j/32;   //compute longitude
+
+                indexedVertices.vertices.push([Math.cos(theta)*Math.sin(phi),Math.cos(phi), Math.sin(theta)*Math.sin(phi)])
+                if (i!==0) { //add a triangle face
+                    indexedVertices.indices.push([i*(32+1)+j, i*(32+1)+j-1, (i-1)*(32+1)+j]);
+                    numberOfVertices+=3;
+                }
+                if (i!==0 && i!==1) { //add an other triangle face
+                    indexedVertices.indices.push([i*(32+1)+j-1, (i-1)*(32+1)+j, (i-1)*(32+1)+j-1]);
+                    numberOfVertices+=3;
+                }
+            }             
+        } 
+
+        // var result = [],
+        //     i,
+        //     j,
+        //     maxi,
+        //     maxj;
+
+        // for (i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+        //     for (j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+        //         result = result.concat(
+        //             indexedVertices.vertices[
+        //                 indexedVertices.indices[i][j]
+        //             ],
+
+        //             indexedVertices.vertices[
+        //                 indexedVertices.indices[i][(j + 1) % maxj]
+        //             ]
+        //         );
+        //     }
+        // }
+
+        // return result;
+
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ]
+                );
+            }
+        }
+
+        return result;   
+
+
+    }
 
 };
