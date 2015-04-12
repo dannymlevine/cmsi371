7c4d246dfe82c326c14eaf633ad0a7fbe8c2e064
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-04-12 02:46:17 -0700

    refactored code
    refactored code to make matrices

--- homework/matrices/matrix.js
+++ homework/matrices/matrix.js
@@ -10,7 +10,8 @@ var Matrix = function(n) {
             }
         }
     }
-    matrix.multiplication = function multiplyMatrices(matrix2) {
+    
+    matrix.multiplication = function multiply(matrix2) {
         var result = [];
         if(matrix[0].length !== matrix2.length){
             throw "Cannot multiply these 2 Matrices"
@@ -45,6 +46,50 @@ var Matrix = function(n) {
             }
             j++
         }
+        return matrix
+    }
+
+    matrix.rotation = function rotation(angle, x, y, z){
+        // In production code, this function should be associated
+        // with a matrix object with associated functions.
+        var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
+            s = Math.sin(angle * Math.PI / 180.0),
+            c = Math.cos(angle * Math.PI / 180.0),
+            oneMinusC = 1.0 - c,
+
+            // We can't calculate this until we have normalized
+            // the axis vector of rotation.
+            x2, // "2" for "squared."
+            y2,
+            z2,
+            xy,
+            yz,
+            xz,
+            xs,
+            ys,
+            zs;
+
+        // Normalize the axis vector of rotation.
+        x /= axisLength;
+        y /= axisLength;
+        z /= axisLength;
+
+        // *Now* we can calculate the other terms.
+        x2 = x * x;
+        y2 = y * y;
+        z2 = z * z;
+        xy = x * y;
+        yz = y * z;
+        xz = x * z;
+        xs = x * s;
+        ys = y * s;
+        zs = z * s;
+
+        // GL expects its matrices in column major order.
+        matrix = [ [(x2 * oneMinusC) + c, (xy * oneMinusC) + zs, (xz * oneMinusC) - ys, 0.0],
+                   [ (xy * oneMinusC) - zs, (y2 * oneMinusC) + c, (yz * oneMinusC) + xs, 0.0],
+                   [ (xz * oneMinusC) + ys, (yz * oneMinusC) - xs, (z2 * oneMinusC) + c, 0.0],
+                   [0.0, 0.0, 0.0, 1.0] ];
         return matrix
     }
 
