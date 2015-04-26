3914d417d288ed46ede88f0e4512713c9cb2b54f
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2015-04-25 23:23:29 -0700

    did revisions
    Made substantial changes to code based on the advice from dondi

--- homework/pipeline/matrices/matrix-test.js
+++ homework/pipeline/matrices/matrix-test.js
@@ -2,118 +2,117 @@ $(function () {
 
     // This suite checks instantiation basics.
     test("Creation", function () {
-        var v = Matrix(4);
 
-        equal(v.length, 4, "Matrix size");
-        equal(v[0][0], 1, "First element of first row");
-        equal(v[1][1], 1, "Second element of second row");
-        equal(v[2][2], 1, "Third element of third row");
-        equal(v[3][3], 1, "Fourth element of fouth row");
-        equal(v[0][1], 0, "Second element of first row");
-        equal(v[2][3], 0, "Third element of fourth row");
+        var v = new matrix(4);
+
+        equal(v.matrixArray.length, 4, "Matrix size");
+        equal(v.matrixArray[0][0], 1, "First element of first row");
+        equal(v.matrixArray[1][1], 1, "Second element of second row");
+        equal(v.matrixArray[2][2], 1, "Third element of third row");
+        equal(v.matrixArray[3][3], 1, "Fourth element of fouth row");
+        equal(v.matrixArray[0][1], 0, "Second element of first row");
+        equal(v.matrixArray[2][3], 0, "Third element of fourth row");
 
     });
 
     test("translation", function () {
-        var v = Matrix(4);
+        var v = new matrix(4);
 
-        equal(v.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
 
         v.translation([1, 2, 3]);
 
-        equal(v[0][3], 1, "Fourth element of first row");
-        equal(v[1][3], 2, "Fourth element of second row");
-        equal(v[2][3], 3, "Fourth element of third row");
-        equal(v[3][3], 1, "Fourth element of fourth row");
+        equal(v.matrixArray[0][3], 1, "Fourth element of first row");
+        equal(v.matrixArray[1][3], 2, "Fourth element of second row");
+        equal(v.matrixArray[2][3], 3, "Fourth element of third row");
+        equal(v.matrixArray[3][3], 1, "Fourth element of fourth row");
 
     });
 
     test("Scale", function () {
-        var v = Matrix(4);
+        var v = new matrix(4);
 
-        equal(v.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
 
         v.scale([2, 3, 4]);
 
-        equal(v[0][0], 2, "First element of first row");
-        equal(v[1][1], 3, "Second element of second row");
-        equal(v[2][2], 4, "Third element of third row");
-        equal(v[3][3], 1, "Fourth element of fourth row");
+        equal(v.matrixArray[0][0], 2, "First element of first row");
+        equal(v.matrixArray[1][1], 3, "Second element of second row");
+        equal(v.matrixArray[2][2], 4, "Third element of third row");
+        equal(v.matrixArray[3][3], 1, "Fourth element of fourth row");
 
     });
 
     test("Multiplication", function () {
-        var v = Matrix(4);
-        var x = Matrix(4);
-        var k = [];
+        var v = new matrix(4);
+        var x = new matrix(4);
+        var k;
 
-        equal(v.length, 4, "Matrix size");
-        equal(x.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
+        equal(x.matrixArray.length, 4, "Matrix size");
 
         // JD: 4(b)
         v.translation([1, 2, 3]);
-        v.scale([2, 3, 4]);
-        x.translation([1, 2, 3]);
         x.scale([2, 3, 4]);
 
         k = v.multiplication(x);
 
-        equal(k[0][3], 3, "Fourth element of first row");
-        equal(k[1][3], 8, "Fourth element of second row");
-        equal(k[2][3], 15, "Fourth element of third row");
-        equal(k[0][0], 4, "First element of first row");
-        equal(k[1][1], 9, "Second element of second row");
-        equal(k[2][2], 16, "Third element of third row");
-        equal(k[3][3], 1, "Fourth element of fourth row");
+        equal(k.matrixArray[0][3], 1, "Fourth element of first row");
+        equal(k.matrixArray[1][3], 2, "Fourth element of second row");
+        equal(k.matrixArray[2][3], 3, "Fourth element of third row");
+        equal(k.matrixArray[0][0], 2, "First element of first row");
+        equal(k.matrixArray[1][1], 3, "Second element of second row");
+        equal(k.matrixArray[2][2], 4, "Third element of third row");
+        equal(k.matrixArray[3][3], 1, "Fourth element of fourth row");
 
     });
 
     test("Rotation", function () {
-        var v = Matrix(4);
+        var v = new matrix(4);
 
-        equal(v.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
 
         v.rotation(90, 2, 3, 4);
 
-        equal(v[0][0], 0.13793103448275865, "First element of first row");
-        equal(v[1][1], 0.3103448275862069, "Second element of second row");
-        equal(v[2][2], 0.5517241379310345, "Third element of third row");
-        equal(v[3][3], 1, "Fourth element of fourth row");
+        equal(v.matrixArray[0][0], 0.13793103448275865, "First element of first row");
+        equal(v.matrixArray[1][1], 0.3103448275862069, "Second element of second row");
+        equal(v.matrixArray[2][2], 0.5517241379310345, "Third element of third row");
+        equal(v.matrixArray[3][3], 1, "Fourth element of fourth row");
 
     });
 
     test("Orthographic", function () {
-        var v = Matrix(4);
+        var v = new matrix(4);
 
-        equal(v.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
 
         v.orthographic(1, 2, 3, 4, 5, 6);
 
-        equal(v[0][0], -2, "First element of first row");
-        equal(v[3][1], 7, "Second element of fourth row");
-        equal(v[3][2], -11, "Third element of fourth row");
-        equal(v[3][3], 1, "Fourth element of fouth row");
+        equal(v.matrixArray[0][0], -2, "First element of first row");
+        equal(v.matrixArray[3][1], 7, "Second element of fourth row");
+        equal(v.matrixArray[3][2], -11, "Third element of fourth row");
+        equal(v.matrixArray[3][3], 1, "Fourth element of fouth row");
 
     });
 
     test("Frustum", function () {
-        var v = Matrix(4);
+        var v = new matrix(4);
 
-        equal(v.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
 
         v.frustum(1, 2, 3, 4, 5, 6);
 
-        equal(v[0][0], -10, "First element of first row");
-        equal(v[1][2], -7, "Third element of second row");
-        equal(v[2][2], -11, "Third element of third row");
+        equal(v.matrixArray[0][0], -10, "First element of first row");
+        equal(v.matrixArray[1][2], -7, "Third element of second row");
+        equal(v.matrixArray[2][2], -11, "Third element of third row");
 
     });
 
     test("toWebGL", function () {
-        var v = Matrix(4);
+        var v = new matrix(4);
         var result = [];
 
-        equal(v.length, 4, "Matrix size");
+        equal(v.matrixArray.length, 4, "Matrix size");
 
         result = v.toWebGL();
 
@@ -124,5 +123,4 @@ $(function () {
 
     });
 
-    // JD: 5(b)
 });
--- homework/pipeline/matrices/matrix.js
+++ homework/pipeline/matrices/matrix.js
@@ -1,157 +1,167 @@
-var Matrix = function (n) {
-    var matrix = [];
-    for (var i = 0; i < n; i++) {
-        matrix[i] = [];
-        for (var j = 0; j < n; j++) {
+var matrix = function (size) {
+    this.matrixArray = [];
+    for (var i = 0; i < size; i++) {
+        this.matrixArray[i] = [];
+        for (var j = 0; j < size; j++) {
             if (i === j) {
-                matrix[i][j] = 1;
+                this.matrixArray[i][j] = 1;
             } else {
-                matrix[i][j] = 0;
+                this.matrixArray[i][j] = 0;
             }
+        }
+
+    }
+};
+
+matrix.prototype.multiplication = function (matrix2) {
+    var result = new matrix(0);
+    if (this.matrixArray[0].length !== matrix2.matrixArray.length) {
+        throw "Cannot multiply these 2 Matrices";
+    } else {
+        for (var i = 0; i < this.matrixArray.length; i++) {
+            result.matrixArray[i] = [];
+            for (var j = 0; j < matrix2.matrixArray[0].length; j++) {
+                var sum = 0;
+                for (var k = 0; k < this.matrixArray[0].length; k++) {
+                    sum += this.matrixArray[i][k] * matrix2.matrixArray[k][j];
+                }
+                result.matrixArray[i][j] = sum;
+            }
+        }
+        return result;
+    }
+},
+
+matrix.prototype.translation = function (array) {
+    matrix1 = [
+        [1, 0, 0, array[0]],
+        [0, 1, 0, array[1]],
+        [0, 0, 1, array[2]],
+        [0.0, 0.0, 0.0, 1.0]
+    ];
+
+    for (var i = 0; i < this.matrixArray.length; i++) {
+        for (var j = 0; j < this.matrixArray.length; j++) {
+            this.matrixArray[i][j] = matrix1[i][j];
         }
     }
 
-    matrix.multiplication = function multiply(matrix2) {
-        var result = [];
-        if (matrix[0].length !== matrix2.length) {
-            throw "Cannot multiply these 2 Matrices";
-        } else {
-            for (var i = 0; i < matrix.length; i++) {
-                result[i] = [];
-                for (var j = 0; j < matrix2[0].length; j++) {
-                    var sum = 0;
-                    for (var k = 0; k < matrix[0].length; k++) {
-                        sum += matrix[i][k] * matrix2[k][j];
-                    }
-                    result[i][j] = sum;
-                }
-            }
-            return result;
-        }
-    };
-
-    // JD: 6(b)
-    matrix.translation = function translation(array) {
-        for (var i = 0; i < array.length; i++) {
-            matrix[i][matrix.length - 1] = array[i];
-        }
-        return matrix;
-    };
-
-    matrix.scale = function scale(scaleSize) {
-        var j = 0;
-        for (var i = 0; i < matrix.length; i++) {
-            matrix[i][j] = scaleSize[i];
-            if (i >= scaleSize.length) {
-                matrix[i][j] = 1;
-            }
-            j++;
-        }
-        return matrix;
-    };
-
-    // JD: 7(b), 8(b)
-    matrix.rotation = function rotation(angle, x, y, z) {
-        var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
-            s = Math.sin(angle * Math.PI / 180.0),
-            c = Math.cos(angle * Math.PI / 180.0),
-            oneMinusC = 1.0 - c,
-
-            // We can't calculate this until we have normalized
-            // the axis vector of rotation.
-            x2, // "2" for "squared."
-            y2,
-            z2,
-            xy,
-            yz,
-            xz,
-            xs,
-            ys,
-            zs;
-
-        // Normalize the axis vector of rotation.
-        x /= axisLength;
-        y /= axisLength;
-        z /= axisLength;
-
-        // *Now* we can calculate the other terms.
-        x2 = x * x;
-        y2 = y * y;
-        z2 = z * z;
-        xy = x * y;
-        yz = y * z;
-        xz = x * z;
-        xs = x * s;
-        ys = y * s;
-        zs = z * s;
-
-        // GL expects its matrices in column major order.
-        matrix1 = [
-            [(x2 * oneMinusC) + c, (xy * oneMinusC) + zs, (xz * oneMinusC) - ys, 0.0],
-            [(xy * oneMinusC) - zs, (y2 * oneMinusC) + c, (yz * oneMinusC) + xs, 0.0],
-            [(xz * oneMinusC) + ys, (yz * oneMinusC) - xs, (z2 * oneMinusC) + c, 0.0],
-            [0.0, 0.0, 0.0, 1.0]
-        ];
-
-        for (var i = 0; i < matrix.length; i++) {
-            for (var j = 0; j < matrix.length; j++) {
-                matrix[i][j] = matrix1[i][j];
-            }
-        }
-
-
-        return matrix;
-    };
-
-    matrix.orthographic = function orthographic(right, left, top, bottom, near, far) {
-        var width = right - left,
-            height = top - bottom,
-            depth = far - near;
-
-        matrix1 = [
-            [2.0 / width, 0.0, 0.0, 0.0],
-            [0.0, 2.0 / height, 0.0, 0.0],
-            [0.0, 0.0, -2.0 / depth, 0.0],
-            [-(right + left) / width, -(top + bottom) / height, -(far + near) / depth, 1.0]
-        ];
-
-        for (var i = 0; i < matrix.length; i++) {
-            for (var j = 0; j < matrix.length; j++) {
-                matrix[i][j] = matrix1[i][j];
-            }
-        }
-
-        return matrix;
-    };
-
-    // JD: 9(b)
-    matrix.frustum = function frustum(right, left, top, bottom, near, far) {
-        var width = right - left,
-            height = top - bottom,
-            depth = far - near;
-
-        matrix1 = [
-            [((2 * near) / width), 0, ((right + left) / width), 0],
-            [0, ((2 * near) / height), ((top + bottom) / height), 0],
-            [0, 0, -(far + near) / depth, -((2 * near * far) / depth)],
-            [0, 0, -1, 0]
-        ];
-
-        for (var i = 0; i < matrix.length; i++) {
-            for (var j = 0; j < matrix.length; j++) {
-                matrix[i][j] = matrix1[i][j];
-            }
-        }
-
-        return matrix;
-    };
-
-    matrix.toWebGL = function webGl() {
-        var result = [];
-        result = result.concat.apply(result, matrix);
-        return result;
-    };
-
-    // JD: 10(b)
     return matrix;
+},
+
+matrix.prototype.scale = function (scaleSize) {
+    matrix1 = [
+        [scaleSize[0], 0, 0, 0],
+        [0, scaleSize[1], 0, 0],
+        [0, 0, scaleSize[2], 0],
+        [0.0, 0.0, 0.0, 1.0]
+    ];
+
+    for (var i = 0; i < this.matrixArray.length; i++) {
+        for (var j = 0; j < this.matrixArray.length; j++) {
+            this.matrixArray[i][j] = matrix1[i][j];
+        }
+    }
+
+    return matrix;
+},
+
+matrix.prototype.rotation = function (angle, x, y, z) {
+    var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
+        s = Math.sin(angle * Math.PI / 180.0),
+        c = Math.cos(angle * Math.PI / 180.0),
+        oneMinusC = 1.0 - c,
+
+        // We can't calculate this until we have normalized
+        // the axis vector of rotation.
+        x2, // "2" for "squared."
+        y2,
+        z2,
+        xy,
+        yz,
+        xz,
+        xs,
+        ys,
+        zs;
+
+    // Normalize the axis vector of rotation.
+    x /= axisLength;
+    y /= axisLength;
+    z /= axisLength;
+
+    // *Now* we can calculate the other terms.
+    x2 = x * x;
+    y2 = y * y;
+    z2 = z * z;
+    xy = x * y;
+    yz = y * z;
+    xz = x * z;
+    xs = x * s;
+    ys = y * s;
+    zs = z * s;
+
+    // GL expects its matrices in column major order.
+    matrix1 = [
+        [(x2 * oneMinusC) + c, (xy * oneMinusC) + zs, (xz * oneMinusC) - ys, 0.0],
+        [(xy * oneMinusC) - zs, (y2 * oneMinusC) + c, (yz * oneMinusC) + xs, 0.0],
+        [(xz * oneMinusC) + ys, (yz * oneMinusC) - xs, (z2 * oneMinusC) + c, 0.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ];
+
+    for (var i = 0; i < this.matrixArray.length; i++) {
+        for (var j = 0; j < this.matrixArray.length; j++) {
+            this.matrixArray[i][j] = matrix1[i][j];
+        }
+    }
+
+
+    return matrix;
+},
+
+matrix.prototype.orthographic = function (right, left, top, bottom, near, far) {
+    var width = right - left,
+        height = top - bottom,
+        depth = far - near;
+
+    matrix1 = [
+        [2.0 / width, 0.0, 0.0, 0.0],
+        [0.0, 2.0 / height, 0.0, 0.0],
+        [0.0, 0.0, -2.0 / depth, 0.0],
+        [-(right + left) / width, -(top + bottom) / height, -(far + near) / depth, 1.0]
+    ];
+
+    for (var i = 0; i < this.matrixArray.length; i++) {
+        for (var j = 0; j < this.matrixArray.length; j++) {
+            this.matrixArray[i][j] = matrix1[i][j];
+        }
+    }
+
+    return matrix;
+},
+
+matrix.prototype.frustum = function (right, left, top, bottom, near, far) {
+    var width = right - left,
+        height = top - bottom,
+        depth = far - near;
+
+    matrix1 = [
+        [((2 * near) / width), 0, ((right + left) / width), 0],
+        [0, ((2 * near) / height), ((top + bottom) / height), 0],
+        [0, 0, -(far + near) / depth, -((2 * near * far) / depth)],
+        [0, 0, -1, 0]
+    ];
+
+    for (var i = 0; i < this.matrixArray.length; i++) {
+        for (var j = 0; j < this.matrixArray.length; j++) {
+            this.matrixArray[i][j] = matrix1[i][j];
+        }
+    }
+
+    return matrix;
+},
+
+matrix.prototype.toWebGL = function () {
+    var result = [];
+    result = result.concat.apply(result, this.matrixArray);
+    return result;
 };
